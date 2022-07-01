0e283c6b27e756fefcdbde98bc23d21382be4548
tingxue <tingxue@pinduoduo.com>
2022-07-01 12:41:16 +0800

    init commit

---
+++ main/status_bar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "status_bar.h"
+
+lv_obj_t* create_status_bar(lv_obj_t* par)
+{
+    return NULL;
+}
---
+++ main/status_bar.h
@@ -0,0 +1,8 @@
+#ifndef __STATUS_BAR_H
+#define __STATUS_BAR_H
+
+#include "lvgl.h"
+
+lv_obj_t* create_status_bar(lv_obj_t* par);
+
+#endif
